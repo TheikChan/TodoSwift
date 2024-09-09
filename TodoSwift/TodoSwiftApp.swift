d7322c62339e4d78362838dd41c5cb5049796fa0
Theik Chan <theik.chan@wavemoney.com.mm>
2024-09-09 21:41:08 +0700

    Initial Commit

---
+++ TodoSwift/TodoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftApp.swift
+//  TodoSwift
+//
+//  Created by Theik Chan on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
